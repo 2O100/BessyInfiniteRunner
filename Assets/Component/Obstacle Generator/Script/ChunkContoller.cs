6ea7006479a563c0612d1a6d2e7fa75244e20b90
Vincent GOUTY <vincent1796@hotmail.fr>
2026-03-25 16:24:41 +0100

    Add Cow Prefab & Chunks
    -Add a Cow Prefab
    -Add an randow spawn chunk system in game

---
+++ Assets/Component/Obstacle Generator/Script/ChunkContoller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ChunkController : MonoBehaviour
+{
+    [SerializeField] private Transform _endAnchor;
+
+    public Vector3 EndAnchor => _endAnchor.position;
+
+    public bool IsBehindPlayer()
+    {
+        return EndAnchor.z <= 0;
+    }
+}
